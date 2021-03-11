3c40820b84aacf8c2ced7e5742fed1f3c284c164
SAURABH SONI <61721761+Mrdynamic-soni@users.noreply.github.com>
2021-03-11 09:01:24 +0530

    Add files via upload
    python code on cloud to get sensor data from esp32 to mysql database

---
+++ espdata.py
@@ -0,0 +1,50 @@
+import socket 
+import time 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host = "localhost",
+  user = "root", 
+  password = "@Godown69",
+  database = "NEW_ESCORTS"  
+)
+
+mycursor =mydb.cursor()
+# mycursor.execute("CREATE DATABASE NEW_ESCORTS")  //to create new databases
+
+# to create tables
+
+# mycursor.execute("CREATE TABLE GYRO(time long, gyroX float, gyroY float, gyroZ float) ");
+
+# mycursor.execute("CREATE TABLE VIBRATION(time long, Hz long)");
+
+# mycursor.execute("CREATE TABLE TEMPERATURE(time long ,Celcius float)");
+
+s=socket.socket()
+s.bind(('0.0.0.0',5767))
+s.listen(8)
+
+
+while True:
+    Time = round(time.time() * 1000)
+    mycursor =mydb.cursor()
+    c,addr = s.accept()
+    
+    data = c.recv(1024).decode("utf-8")
+    DATA = data.split()
+    print(DATA)
+    
+    q1 = "INSERT INTO TEMPERATURE(time,Celcius) VALUES(%s,%s)"
+    val1 = (Time ,DATA[0])
+    mycursor.execute(q1,val1)
+    q2 = "INSERT INTO VIBRATION(time,Hz) VALUES(%s,%s)"
+    val2= (Time,DATA[1])
+    mycursor.execute(q2,val2)
+
+    q3 = "INSERT INTO GYRO(time, gyroX, gyroY, gyroZ) VALUES(%s,%s,%s,%s)"
+    val3= (Time,DATA[5],DATA[6],DATA[7])
+    mycursor.execute(q3,val3)
+    mydb.commit()
+    time.sleep(1)
+    print("RECORD INSRTED")
+  
